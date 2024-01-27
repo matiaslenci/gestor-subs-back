1db791740ee50db7d6a9a32fb2dcb16af9075882
Matias Lenci <matiaslenci@gmail.com>
2024-01-27 14:52:00 -0300

    add create dto color y defaultSub

--- src/color/entities/color.entity.ts
+++ src/color/entities/color.entity.ts
@@ -7,6 +7,7 @@ export class Color {
 
   @Column('text', {
     unique: true,
+    default: 'gris',
   })
   name: string;
 }
--- src/default-sub/dto/create-default-sub.dto.ts
+++ src/default-sub/dto/create-default-sub.dto.ts
@@ -1,1 +1,17 @@
-export class CreateDefaultSubDto {}
+import { IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateDefaultSubDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsString()
+  @MinLength(1)
+  @IsOptional()
+  logo?: string;
+
+  @IsString()
+  @MinLength(1)
+  @IsOptional()
+  slug?: string;
+}
