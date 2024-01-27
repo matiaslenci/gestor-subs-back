c472a2c638e74b83d895bb7cc21c58ce07ab72f0
Matias Lenci <matiaslenci@gmail.com>
2024-01-27 14:11:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+[circleci-image]: https://img.shields.io/circleci/build/github/nestjs/nest/master?token=abc123def456
+[circleci-url]: https://circleci.com/gh/nestjs/nest
+
+ 
+# Gestor de Subs API
+
+1. Clonar proyecto
+2. ``` pnpm install ```
+3. Clonar el archivo ```.env.template``` y renombralo a ```.env```
+4. Cambiar las variables de entorno
+5. Levatar base de datos
+```bash
+$ docker-compose up -d
+```
+6.Correr la aplicación
+```bash
+$ pnpm run start:dev
+```
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // Cuando creamos un cambio en nuestras entidades automaticamente las sincroniza
+      //! En prod synchronize usualmente va en false, deberiamos usar una migración
+    }),
+  ],
+})
+export class AppModule {}
