73544493f5a250e206c05d75db0c8a47b1548908
Matias Lenci <matiaslenci@gmail.com>
2024-01-28 21:02:27 -0300

    add beforeUpdate en defaultSub entity

--- src/default-sub/entities/default-sub.entity.ts
+++ src/default-sub/entities/default-sub.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class DefaultSub {
@@ -43,5 +49,14 @@ export class DefaultSub {
 
       this.logo = iniciales.toUpperCase();
     }
+  }
+
+  @BeforeUpdate()
+  checkSlugUpdate() {
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' +', '')
+      .replaceAll("'", '')
+      .replaceAll(' ', '_');
   }
 }
