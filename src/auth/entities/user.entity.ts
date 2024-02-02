f15b7efc2ab3b3ca2becbee904f055f9fa788563
Matias Lenci <matiaslenci@gmail.com>
2024-02-02 19:52:32 -0300

    add entidad usuario

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column('text')
+  fullName: string;
+
+  @Column('text')
+  avatar: string;
+
+  @Column('bool') // Postgres utiliza bool
+  isActive: boolean;
+
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  roles: string[];
+}
